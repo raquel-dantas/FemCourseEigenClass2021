7279c1f63bd7e1cd0555867293ae10fb6136ede1
raquel-dantas <raqueldantasbatista@gmail.com>
2021-05-23 19:13:05 -0300

    Shape quadrilateral up to quadratic

--- sources/ShapeQuad.cpp
+++ sources/ShapeQuad.cpp
@@ -25,15 +25,50 @@ void ShapeQuad::Shape(const VecDouble &xi, VecInt &orders, VecDouble &phi, Matri
         DebugStop();
     }
 
-    auto nf = NShapeFunctions(orders);
+    int nshape = NShapeFunctions(orders);
+    phi.resize(nshape);
+    dphi.resize(2,nshape);
 
-    if (orders[nf-1] > 2) {
+    if (orders[nshape-1] > 2) {
         std::cout << "ShapeQuad::Shape, only implemented until order = 2" << std::endl;
         DebugStop();
     }
+    // Linear
+    phi[0] = 1./4.*(1. - xi[0])*(1. - xi[1]);
+    phi[1] = 1./4.*(1. + xi[0])*(1. - xi[1]);
+    phi[2] = 1./4.*(1. + xi[0])*(1. + xi[1]);
+    phi[3] = 1./4.*(1. - xi[0])*(1. + xi[1]);
 
-    std::cout << "Please implement me\n";
-    DebugStop();
+    // dphi(i,j) represents the ith derivative of function j
+    dphi(0,0) = -1./4.*(1. - xi[1]);
+    dphi(1,0) = -1./4.*(1. - xi[0]);
+    dphi(0,1) = 1./4.*(1. - xi[1]);
+    dphi(1,1) = -1./4.*(1. + xi[0]);
+    dphi(0,2) = 1./4.*(1. + xi[1]);
+    dphi(1,2) = 1./4.*(1. + xi[0]);
+    dphi(0,3) = -1./4.*(1. + xi[1]);
+    dphi(1,3) = 1./4.*(1. - xi[0]);
+
+    // Quadratic
+    if(nshape > 4){
+        phi[4] = (orders[4] >= 2)*1./2.*(1. - xi[0]*xi[0])*(1. - xi[1]);
+        phi[5] = (orders[5] >= 2)*1./2.*(1. - xi[1]*xi[1])*(1. + xi[0]);
+        phi[6] = (orders[6] >= 2)*1./2.*(1. - xi[0]*xi[0])*(1. + xi[1]);
+        phi[7] = (orders[7] >= 2)*1./2.*(1. - xi[1]*xi[1])*(1. - xi[0]);
+        phi[8] = (orders[8] >= 2)*(1. - xi[0]*xi[0])*(1. - xi[1]*xi[1]);
+
+        dphi(0,4) = (orders[4] >= 2) * xi[0]*(-1. + xi[1]);
+        dphi(1,4) = (orders[4] >= 2) * -1./2.*(1. - xi[0]*xi[0]);
+        dphi(0,5) = (orders[5] >= 2) * 1./2.*(1. - xi[1]*xi[1]);
+        dphi(1,5) = (orders[5] >= 2) * -xi[1]*(1. + xi[0]);
+        dphi(0,6) = (orders[6] >= 2) * -xi[0]*(1. + xi[1]);
+        dphi(1,6) = (orders[6] >= 2) * 1./2.*(1. - xi[0]*xi[0]);
+        dphi(0,7) = (orders[7] >= 2) * -1./2.*(1. - xi[1]*xi[1]);
+        dphi(1,7) = (orders[7] >= 2) * -xi[1]*(1. - xi[0]);
+        dphi(0,8) = (orders[8] >= 2) * -2.*xi[0]*(1. - xi[1]*xi[1]);
+        dphi(1,8) = (orders[8] >= 2) * -2.*xi[1]*(1. - xi[0]*xi[0]);
+    }
+ 
 }
 
 /// returns the number of shape functions associated with a side
