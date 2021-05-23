9e0bec7ce0eab726a3046a0e5991f47274c662e1
raquel-dantas <raqueldantasbatista@gmail.com>
2021-05-23 19:13:19 -0300

    Shape triangle up to quadratic

--- sources/ShapeTriangle.cpp
+++ sources/ShapeTriangle.cpp
@@ -25,9 +25,11 @@ void ShapeTriangle::Shape(const VecDouble &xi, VecInt &orders, VecDouble &phi, M
         DebugStop();
     }
 
-    auto nf = NShapeFunctions(orders);
+    int nshape = NShapeFunctions(orders);
+    phi.resize(nshape);
+    dphi.resize(2,nshape);
 
-    if (orders[nf-1] > 2) {
+    if (orders[nshape-1] > 2) {
         std::cout << "ShapeTriangle::Shape, only implemented until order = 2" << std::endl;
         DebugStop();
     }
@@ -36,6 +38,8 @@ void ShapeTriangle::Shape(const VecDouble &xi, VecInt &orders, VecDouble &phi, M
     phi[0] =  1.-xi[0]-xi[1];
     phi[1] =  xi[0];
     phi[2] =  xi[1];
+
+    // dphi(i,j) represents the ith derivative of function j
     dphi(0,0) = -1.;
     dphi(1,0) = -1.;
     dphi(0,1) =  1.;
@@ -43,9 +47,29 @@ void ShapeTriangle::Shape(const VecDouble &xi, VecInt &orders, VecDouble &phi, M
     dphi(0,2) =  0.;
     dphi(1,2) =  1.;
     
-    std::cout << "Please implement me\n";
-    DebugStop();
+    // Quadratic
+    if(nshape > 3){
+        
+        phi[3] = (orders[3] >= 2) * 4 * phi[0] * phi[1];
+        phi[4] = (orders[4] >= 2) * 4 * phi[1] * phi[2];
+        phi[5] = (orders[5] >= 2) * 4 * phi[0] * phi[2];
+        
+        dphi(0,3) = (orders[4] >= 2) * -4*(-1 + xi[1] + 2*xi[0]);
+        dphi(1,3) = (orders[4] >= 2) * -4*xi[0];
+        dphi(0,4) = (orders[4] >= 2) * 4*xi[1];
+        dphi(1,4) = (orders[4] >= 2) * 4*xi[0];
+        dphi(0,5) = (orders[5] >= 2) * -4*xi[1];
+        dphi(1,5) = (orders[5] >= 2) * -4*(-1 + 2*xi[1] + xi[0]);
+        
+        
+    }
     
+    //Cubic
+    if(orders[nSides - 1] >= 3){
+        phi[6] = (orders[6] >= 2) * 27 *phi[0] * phi[1] * phi[2];
+        dphi(0,6) = (orders[6] >= 2) * -27*xi[1]*(-1 + xi[1] + 2*xi[0]);
+        dphi(1,6) = (orders[6] >= 2) * -27*xi[0]*(-1 + 2*xi[1] + xi[0]);
+    }
     
 }
 
