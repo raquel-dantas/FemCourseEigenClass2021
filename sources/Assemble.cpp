e14947bcc767389183710bd748eff414744894bc
raquel-dantas <raqueldantasbatista@gmail.com>
2021-06-28 16:25:56 -0300

    Assembly of global matrix

--- sources/Assemble.cpp
+++ sources/Assemble.cpp
@@ -46,30 +46,87 @@ void Assemble::OptimizeBandwidth() {
 }
 
 void Assemble::Compute(MatrixDouble &globmat, MatrixDouble &rhs) {
-    
+    // Number of equations from the role mesh
     auto neq = NEquations();
-    
+    // Setup of the global matrix and rhs
     globmat.resize(neq, neq);
     globmat.setZero();
     rhs.resize(neq, 1);
     rhs.setZero();
     
+    // Loop over computational elements
     int64_t nelem = cmesh->GetGeoMesh()->NumElements();
     for (int el = 0; el < nelem; el++) {
         CompElement *cel = cmesh->GetElement(el);
-
+        // Number of shape functions defined over the element
         int nshape = cel->NShapeFunctions();
+        // Number of state variables (1 for scalar, 3 for vectors)
         int nstate = cel->GetStatement()->NState();
+        // Setup element stiffness matrix
         MatrixDouble ek(nstate * nshape, nstate * nshape);
         MatrixDouble ef(nstate * nshape, 1);
         ek.setZero();
         ef.setZero();
-
+        //Compute element stiffness matrix and load vector
         cel->CalcStiff(ek, ef);
+
+        // Loop over all DOFs
+        int ndofs = cel->NDOF();
+        for(int i = 0; i < ndofs; i++){
+            // Get the DOF of i index
+            DOF idof = cel->GetDOF(i);
+            // Number of shape functions of the i index DOF (a DOF can have more than one shape functions)
+            int nfun_i = idof.GetNShape();
+            int64_t ifeq = idof.GetFirstEquation();
+            // Loop over functions grouped in the DOF
+            for(int ii = 0; ii < nfun_i*nstate; ii++){
+                // Local index of the element matrix coefficient
+                int iLocal = i + ii;
+                // Global index associated to iLocal
+                int iGlobal = ifeq + ii;
+                
+                // Add ef to the global rhs vector
+                rhs(iGlobal,0) += ef(iLocal,0);
+
+                // Loop over DOFs
+                for(int j = 0; j < ndofs; j++){
+                    // Get the DOF of j index
+                    DOF jdof = cel->GetDOF(j);
+                    // Number of shape functions of the j index DOF (a DOF can have more than one shape functions)
+                    int nfun_j = jdof.GetNShape();
+                    int64_t jfeq = jdof.GetFirstEquation();
+                    // Loop over functions grouped in the DOF
+                    for(int jj = 0; jj < nfun_j*nstate; jj++){
+                        // Local index of the element matrix coefficient
+                        int jLocal = j + jj;
+                        // Global index associated to jLocal
+                        int jGlobal = jfeq + jj;
+
+                        // Add ek to the global stiffness matrix
+                        globmat(iGlobal,jGlobal) += ek(iLocal,jLocal);
+
+                    }
+                }
+            }
+        }
         
-        //+++++++++++++++++
-        std::cout << "\nPLEASE IMPLEMENT ME\n" << __PRETTY_FUNCTION__ << std::endl;
-        DebugStop();
-        //+++++++++++++++++
     }
 }
+
+/**
+        VecInt globalindex(ek.rows());
+
+        int ndofs = cel->NDOF();
+        for(int i = 0; i < ndofs; i++){
+            DOF idof = cel->GetDOF(i);
+            int nfun_i = idof.GetNShape();
+            int64_t ifeq = idof.GetFirstEquation();
+
+            for(int ii = 0; ii < nfun_i; ii++){
+                int iLocal = i+ii;
+                int iGlobal = ifeq + ii;
+                globalindex[iLocal] = iGlobal;
+
+            }
+        }
+        */
